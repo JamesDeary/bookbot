1cd571643d2207a3dfac664a394d331996c3bea0
JamesDeary <jamiedeary@gmail.com>
2024-06-17 22:50:47 +0100

    Updated main.py to convert single dictionary into a list of them which gets sorted. A report of the results is printed.

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 # define main function
 def main():
-    #path defined in "" as the file is a string
+    # path defined in "" as the file is a string
     path_to_file = "/home/jd/workspace/github.com/JamesDeary/bookbot/book/frankenstein.txt"
 
-    #open the file and name f, so file can be read and print its contents
+    # open the file and name f, so file can be read and print its contents
     with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
+        #print(file_contents)
 
-    #call function word count
+    # call function word count
     total_words = word_count(file_contents)
-    print(f"The number of words in this document = {total_words}")
+    #print(f"The number of words in this document = {total_words}")
 
-    #call character count function
+    # call character count function
     total_characters = character_count(file_contents)
-    print(f"The character count is as follows: {total_characters}")
+    #print(f"The character count is as follows: {total_characters}")
+
+    # call convert_dict
+    converted_dict = convert_dict(total_characters)
+    #print(converted_dict)
+
+    # sort newly converted dictionary
+    converted_dict.sort(reverse=True, key=sort_on)
+    #print(converted_dict)
+
+    # begin printing report
+    print(f"--- Being report of {path_to_file} ---")
+    print(f"{total_words} words found in the document")
+    print("")
+    for a in converted_dict:
+        for key, value in a.items():
+            print(f"The character {key} was found {value} times.")
+
+    print("--- End report ---")
 
 # define a function to count words in file
 def word_count(file_contents):
@@ -23,26 +41,39 @@ def word_count(file_contents):
 
 # define a function to count characters in file
 def character_count(file_contents):
-    #convert file_contents to lowercase
+
+    # convert file_contents to lowercase
     lowered_string = file_contents.lower()
     new_dict = {}
-    #split lowered words into list of lowered words
+
+    # split lowered words into list of lowered words
     lowered_words = lowered_string.split()
     new_list = list()
 
-    #get list of words into list of characters
+    # get list of words into list of characters
     for a in lowered_words:
         new_list+=a
 
-    #iterate over list of characters and store character as key and value as total number of them
+    # iterate over list of characters and store character as key and value as total number of them
     for a in new_list:
-        if a in new_dict:
-            new_dict[a] +=1
-        elif a not in new_dict:
-            new_dict[a] = 1
-
+        if a.isalpha(): 
+            if a in new_dict:
+                new_dict[a] +=1
+            elif a not in new_dict:
+                new_dict[a] = 1
     return (new_dict)
 
 
-#call main function
+# define a function to convert single dictionary into list of dicts, not tuples - iteration matters
+def convert_dict(total_characters):
+    new_list = list({key: value} for key, value in total_characters.items())
+    return (new_list)
+
+# define a function to sort on       
+def sort_on(converted_dict):
+    return list(converted_dict.values())[0]
+
+
+
+# call main function
 main()
